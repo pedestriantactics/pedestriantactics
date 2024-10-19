e059e61aa54fe183c232c7baceda6860ee9eb262
pedestriantactics <pedestriantactics@gmail.com>
2024-10-19 20:03:08 +0000

    edits to prerender in slug

--- src/routes/releases/[slug]/+page.ts
+++ src/routes/releases/[slug]/+page.ts
@@ -1,4 +1,4 @@
-export const prerender = true;
+export const prerender = false;
 
 export async function load({ params }: { params: { slug: string } }) {
     const post = await import(`../${params.slug}.md`)
