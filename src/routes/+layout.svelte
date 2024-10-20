8f698ea85fad3a17c272105dfbc35b8295ca45fc
Dan <hi@pedestriantactics.com>
2024-10-20 11:49:42 -0700

    quicker delay

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,51 @@
 <script lang="ts">
-    import "../reset.css";
-    import "../styles.css";
+	import "../reset.css";
+	import "../styles.css";
 
-    import { onMount, afterUpdate } from "svelte";
+	import { onMount, afterUpdate } from "svelte";
 
-    export function animateElements(duration = 0, delay = 10) {
-        const elements = document.querySelectorAll(
-            "*:not(.no-animate):not(.no-animate *)",
-        );
-        elements.forEach((element, index) => {
-            const elementDelay = index * delay;
-            setTimeout(() => {
-                console.log(
-                    `Animating element: ${element.tagName}, index: ${index}, delay: ${elementDelay}`,
-                );
-                const animation = element.animate(
-                    [{ opacity: 0 }, { opacity: 1 }],
-                    {
-                        duration,
-                        fill: "forwards", // Ensure the element stays visible after the animation
-                    },
-                );
-                animation.onfinish = () => {
-                    (element as HTMLElement).style.opacity = "1";
-                };
-            }, elementDelay); // adjust delay for each element
-        });
-    }
+	export function animateElements(duration = 0, delay = 14) {
+		const elements = document.querySelectorAll(
+			"body div:not(.no-animate):not(.no-animate *), body img:not(.no-animate):not(.no-animate *), body p:not(.no-animate):not(.no-animate *), body h1:not(.no-animate):not(.no-animate *), body h2:not(.no-animate):not(.no-animate *), body h3:not(.no-animate):not(.no-animate *), body h4:not(.no-animate):not(.no-animate *)",
+		);
+		elements.forEach((element, index) => {
+			const elementDelay = index * delay;
+			setTimeout(() => {
+				console.log(
+					`Animating element: ${element.tagName}, index: ${index}, delay: ${elementDelay}`,
+				);
+				const animation = element.animate(
+					[{ opacity: 0 }, { opacity: 1 }],
+					{
+						duration,
+						fill: "forwards", // Ensure the element stays visible after the animation
+					},
+				);
+				animation.onfinish = () => {
+					(element as HTMLElement).style.opacity = "1";
+				};
+			}, elementDelay); // adjust delay for each element
+		});
+	}
 
-    function setInitialOpacity() {
-        const elements = document.querySelectorAll(
-            "*:not(.no-animate):not(.no-animate *)",
-        );
-        elements.forEach((element) => {
-            (element as HTMLElement).style.opacity = "0";
-        });
-    }
+	function setInitialOpacity() {
+		const elements = document.querySelectorAll(
+			"body div:not(.no-animate):not(.no-animate *), body img:not(.no-animate):not(.no-animate *), body p:not(.no-animate):not(.no-animate *), body h1:not(.no-animate):not(.no-animate *), body h2:not(.no-animate):not(.no-animate *), body h3:not(.no-animate):not(.no-animate *), body h4:not(.no-animate):not(.no-animate *)",
+		);
+		elements.forEach((element) => {
+			(element as HTMLElement).style.opacity = "0";
+		});
+	}
 
-    onMount(() => {
-        setInitialOpacity();
-        animateElements();
-    });
+	onMount(() => {
+		setInitialOpacity();
+		animateElements();
+	});
 
-    afterUpdate(() => {
-        setInitialOpacity();
-        animateElements();
-    });
+	afterUpdate(() => {
+		setInitialOpacity();
+		animateElements();
+	});
 </script>
 
 <slot />
