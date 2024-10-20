64ad5b15c51303b0e18bc41e4e0796d2c0489a59
Dan <hi@pedestriantactics.com>
2024-10-20 11:34:39 -0700

    delay changes for animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,51 @@
 <script lang="ts">
-	import "../reset.css";
-	import "../styles.css";
+    import "../reset.css";
+    import "../styles.css";
 
-	import { onMount, afterUpdate } from "svelte";
+    import { onMount, afterUpdate } from "svelte";
 
-	export function animateElements(duration = 0, delay = 15) {
-		const elements = document.querySelectorAll(
-			"*:not(.no-animate):not(.no-animate *)",
-		);
-		elements.forEach((element, index) => {
-			setTimeout(() => {
-				console.log(
-					`Animating element: ${element.tagName}, index: ${index}`,
-				);
-				const animation = element.animate(
-					[{ opacity: 0 }, { opacity: 1 }],
-					{
-						duration,
-						fill: "forwards", // Ensure the element stays visible after the animation
-					},
-				);
-				animation.onfinish = () => {
-					(element as HTMLElement).style.opacity = "1";
-				};
-			}, index * delay); // adjust delay for each element
-		});
-	}
+    export function animateElements(duration = 0, delay = 10) {
+        const elements = document.querySelectorAll(
+            "*:not(.no-animate):not(.no-animate *)",
+        );
+        elements.forEach((element, index) => {
+            const elementDelay = index === 0 ? 0 : index * delay;
+            setTimeout(() => {
+                console.log(
+                    `Animating element: ${element.tagName}, index: ${index}`,
+                );
+                const animation = element.animate(
+                    [{ opacity: 0 }, { opacity: 1 }],
+                    {
+                        duration,
+                        fill: "forwards", // Ensure the element stays visible after the animation
+                    },
+                );
+                animation.onfinish = () => {
+                    (element as HTMLElement).style.opacity = "1";
+                };
+            }, elementDelay); // adjust delay for each element
+        });
+    }
 
-	function setInitialOpacity() {
-		const elements = document.querySelectorAll(
-			"*:not(.no-animate):not(.no-animate *)",
-		);
-		elements.forEach((element) => {
-			(element as HTMLElement).style.opacity = "0";
-		});
-	}
+    function setInitialOpacity() {
+        const elements = document.querySelectorAll(
+            "*:not(.no-animate):not(.no-animate *)",
+        );
+        elements.forEach((element) => {
+            (element as HTMLElement).style.opacity = "0";
+        });
+    }
 
-	onMount(() => {
-		setInitialOpacity();
-		animateElements();
-	});
+    onMount(() => {
+        setInitialOpacity();
+        animateElements();
+    });
 
-	afterUpdate(() => {
-		setInitialOpacity();
-		animateElements();
-	});
+    afterUpdate(() => {
+        setInitialOpacity();
+        animateElements();
+    });
 </script>
 
 <slot />
