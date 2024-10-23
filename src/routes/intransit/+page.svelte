b589f9565b1fea9dd266e29ffbf4251d9bd68382
Dan <hi@pedestriantactics.com>
2024-10-23 12:36:20 -0700

    in transit beginning page

---
+++ src/routes/intransit/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	// import the textlayout from the lib folder
+	import NavMenu from "$lib/NavMenu.svelte";
+	import ImageContainer from "./ImageContainer.svelte";
+</script>
+
+<NavMenu
+	currentPage="In Transit"
+	breadcrumbs={[
+		{ name: "Releases", destination: "/releases" },
+		{ name: "PT-A7", destination: "/releases/pt-a7" },
+	]}
+/>
+
+<div id="container">
+	<div id="content">
+		<ImageContainer imageFileName="in-transit-photo-series-1.jpg" date="2020-20-20" name="pt-a7-img1" description="ISO 50 / F1.4 / S1/120" />
+		<ImageContainer imageFileName="in-transit-photo-series-1.jpg" date="2020-20-20" name="pt-a7-img1" description="ISO 50 / F1.4 / S1/120" />
+	</div>
+</div>
+
+<svelte:head>
+	<title>In Transit - Pedestrian Tactics</title>
+	<style>
+		:root {
+			--color-fg: black;
+			--color-bg: white;
+		}
+	</style>
+</svelte:head>
+
+<style>
+	#container {
+		max-width: 960px;
+		width: 100vw;
+		height: 100vh;
+		display: grid;
+		align-items: center;
+		justify-items: center;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	#content {
+		padding: 2rem;
+	}
+</style>
---
+++ src/routes/intransit/ImageContainer.svelte
@@ -0,0 +1,31 @@
+<script>
+	export var imageFileName = "";
+	export var date = "";
+	export var name = "";
+	export var description = "";
+</script>
+
+<div id="image-container">
+	<img alt={name} src="images/{imageFileName}" />
+	<div id="image-footer">
+		<p class="caption mono">{date}</p>
+		<p></p>
+		<p></p>
+		<p class="caption mono">{name}</p>
+		<p class="caption mono">{description}</p>
+	</div>
+</div>
+
+<style>
+	#image-container {
+		 margin-bottom: 3rem;
+	}
+	#image-footer {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+	}
+	img {
+		width: 100%;
+		margin-bottom: 1rem;
+	}
+</style>
