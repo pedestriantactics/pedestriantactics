34a5310b956d8e07637ee6892b1e591cf0a8b9c9
Dan <hi@pedestriantactics.com>
2024-11-11 10:54:09 -0800

    text editor

--- src/lib/NavMenu.svelte
+++ src/lib/NavMenu.svelte
@@ -2,6 +2,7 @@
 	import { page } from "$app/stores";
 	export let currentPage: string;
 	export let breadcrumbs: { name: string; destination: string }[];
+	export let showFullMenu = true;
 
 	let links: { name: string; destination: string }[] = [
 		{ name: "Releases", destination: "/releases" },
@@ -16,20 +17,25 @@
 
 <div id="nav-top" class="no-animate">
 	<p class="caption mono">
-		<a class="unstyled" href="../">Pedestrian Tactics</a> / {#each breadcrumbs as breadcrumb}<a class="unstyled"
+		<a class="unstyled" href="../">Pedestrian Tactics</a> / {#each breadcrumbs as breadcrumb}<a
+				class="unstyled"
 				href={breadcrumb.destination}>{breadcrumb.name}</a
 			> /
 		{/each}{currentPage}
-		<br />
-		<br />
-		{#each links as link}
-			{#if link.name === currentPage}
-				{link.name}←
-			{:else}
-				<a class="linky unstyled" href={link.destination}>{link.name}</a>
-			{/if}
+		{#if showFullMenu}
 			<br />
-		{/each}
+			<br />
+			{#each links as link}
+				{#if link.name === currentPage}
+					{link.name}←
+				{:else}
+					<a class="linky unstyled" href={link.destination}
+						>{link.name}</a
+					>
+				{/if}
+				<br />
+			{/each}
+		{/if}
 	</p>
 </div>
 
@@ -46,7 +52,6 @@
 	.linky:focus {
 		text-decoration: none;
 	}
-
 
 	.linky:hover::after {
 		content: "←";
