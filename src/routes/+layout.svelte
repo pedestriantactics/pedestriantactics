1a159c2eb49a18151cb1ccf488cc8443536c3936
Dan <hi@pedestriantactics.com>
2025-04-09 14:06:16 -0700

    prevent analytics when devving

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -154,7 +154,9 @@
 </script>
 
 <svelte:head>
+	{#if import.meta.env.PROD}
 	<script defer src="https://umami.catalystsoftworks.com/script.js" data-website-id="b7828a5d-9421-478b-9e4a-0dc218da5435"></script>
+	{/if}
 </svelte:head>
 
 <slot />
