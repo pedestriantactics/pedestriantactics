1be13f0ce4ae2592d6b9db6d1d6ef7accf9e70c7
Dan <hi@pedestriantactics.com>
2025-10-27 22:34:44 -0700

    animation changes

--- src/routes/sessions/+page.svelte
+++ src/routes/sessions/+page.svelte
@@ -8,15 +8,15 @@
 
 <div id="container">
 	<div id="content">
-		<h2>
+		<h2 class="">
 			I offer one-on-one sessions for anyone who would like to improve
 			their music production process. In the past these sessions have
 			ranged from birds-eye discussions on mindset and overcoming production hurdles, 
 			to technical sound design tutorials and critiques. All skill levels and
 			softwares are welcome
 		</h2>
-		<h2>Click the arrow to be redirected to my Calendly booking page</h2>
-		<h2><a class="unstyled-link" href="https://calendly.com/pedestriantactics/1-hour">→</a></h2>
+		<h2 class="">Click the arrow to be redirected to my Calendly booking page</h2>
+		<h2 class="animate"><a class="unstyled-link" href="https://calendly.com/pedestriantactics/1-hour">→</a></h2>
 	</div>
 </div>
 
@@ -24,18 +24,20 @@
 	<title>Sessions - Pedestrian Tactics</title>
 </svelte:head>
 
-<!-- <style>
+<style>
 	#content {
 		/* this helps with animation */
-		min-height: 280pt;
+		min-height: 350pt;
 		/* background-color: blue; */
 		/* height: auto; */
 	}
 
-	@media (max-height: 400px), (max-width: 800px) and (max-height: 2000px) {
+	
+</style>
+
+<!-- @media (max-height: 400px), (max-width: 800px) and (max-height: 2000px) {
 		#container {
 			align-items: flex-start;
 			margin-top: var(--top-margin);
 		}
-	}
-</style> -->
+	} -->
